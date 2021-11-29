2d0354af79f7e42d9e0303b964802e32d729296d
Ivan Cmelo <cmelo.ivan@gmail.com>
2021-11-29 15:02:44 +0100

    Minor docstring additions to chem module

--- fip/chem.py
+++ fip/chem.py
@@ -5,20 +5,45 @@ from rdkit.Chem.BRICS import BRICSDecompose
 
 
 def smiles2rdmol(smiles):
+    """Simple conversion of SMILES string into a RDKit Mol instance.
+    Wrapped in case some standardization/postprocessing needed.
+
+    :param smiles: SMILES string
+    :return: RDKit Mol instance
+    """
     return Chem.MolFromSmiles(smiles)
 
 
 def smarts2rdmol(smarts):
+    """Simple conversion of SMARTS string into a RDKit Mol instance.
+    Wrapped in case some standardization/postprocessing needed.
+
+    :param smarts: SMARTS string
+    :return: RDKit Mol instance
+    """
     return Chem.MolFromSmarts(smarts)
 
 
 def rdmol_has_substructure_pattern(rdmol, pattern):
+    """Direct wrap of RDKit HasSubstructMatch.
+    Wrapped in case some standardization/postprocessing needed.
+
+    :param rdmol: RDKit Mol instance to search the pattern in
+    :param pattern: the pattern to search, also in RDKit Mol form
+    :return: bool or None (in case of RDKit Mol instance error)
+    """
     if not rdmol:
         return None
     return rdmol.HasSubstructMatch(pattern)
 
 
 def sdf2rdmols(sdf_path):
+    """Converts an SDF file to RDKit Mol instances.
+    Wrap of RDKit SDMolSupplier, modified to omit RDKit Mol instances that can't be parsed.
+
+    :param sdf_path: Path to the SDF file
+    :return: a generator yielding RDKit Mol instances
+    """
     supplier = Chem.SDMolSupplier(sdf_path)
     for rdmol in supplier:
         if rdmol:
@@ -26,6 +51,14 @@ def sdf2rdmols(sdf_path):
 
 
 def rdmol2fragment_smiles(mol, fragment_locations, min_radius=0):
+    """Generates a set of fragments in SMILES notation from a molecule given as an RDKit Mol instance,
+    based on provided atom indices and radii.
+
+    :param mol: the molecule for fragmenting, as RDKit Mol
+    :param fragment_locations: an iterable of fragment locations, in (atom, radius) tuples
+    :param min_radius: minimal feature radius to consider, default 0. Can be set to ignore lower-scope features.
+    :return: a set of SMILES strings
+    """
     fragment_smiles = set()
     for atom, radius in fragment_locations:
         if radius < min_radius:
@@ -45,6 +78,13 @@ def rdmol2fragment_smiles(mol, fragment_locations, min_radius=0):
 
 
 def rdmol2morgan_feature_smiles(mol, radius=3, min_radius=1):
+    """Breaks a molecule, given as an RDKit Mol instance, into a set of ECFP-like fragments with selected radius, and returns them in SMILES notation.
+
+    :param mol: the molecule for fragmenting, as RDKit Mol
+    :param radius: EC fragment radius
+    :param min_radius: minimal fragment radius to consider, default 0. Can be set to ignore lower-scope fragments.
+    :return: a set of SMILES strings
+    """
     bit_info = {}
     features = set()
     Chem.GetMorganFingerprint(mol, radius, bitInfo=bit_info)
